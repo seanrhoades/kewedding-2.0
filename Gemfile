6c501b7a52ee5e6b8a4b260fabb5668c5b96d205
Sean Rhoades <sean@seanrhoades.com>
2016-03-30 22:15:10 -0400

    added aws for paperclip uploads

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.5.1'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
+# Use AWS for media storage
+gem 'aws-sdk'
 # Use bootstrap for front-end
 gem 'bootstrap-sass'
 # Use SCSS for stylesheets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  #Use AWS for paperclip uploads
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_host_name => 's3-website-us-east-1.amazonaws.com',
+    :s3_credentials => {
+      :bucket => 'kewedding'
+      }
+    }
+
 end
