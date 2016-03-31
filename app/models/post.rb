294ab4ea4333ac7dacd068b5e1742fdaa28207fc
Sean Rhoades <sean@seanrhoades.com>
2016-03-31 15:30:21 -0400

    aws test 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ActiveRecord::Base
     validates :user_id, presence: true
     validates :image, presence: true
 
-    has_attached_file :image, styles: { :medium => "640x" }
+    has_attached_file :image,
+          styles: { :medium => "640x" }
+          :storage => :s3,
+          :bucket  => ENV['AWS_S3_BUCKET']
+
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
     has_many :comments, dependent: :destroy
