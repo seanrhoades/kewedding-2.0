b41b571ab2ef2aa9f97e2f92145c62455daafda4
Sean Rhoades <sean@seanrhoades.com>
2016-03-31 14:53:57 -0400

    aws test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,12 @@ Rails.application.configure do
 
   #Use AWS for paperclip uploads
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_host_name => 's3-website-us-east-1.amazonaws.com',
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_S3_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
 
 end
