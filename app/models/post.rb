f2e21192de2069fedfc4088001559215da67e60d
Sean Rhoades <sean@seanrhoades.com>
2016-03-31 14:56:35 -0400

    aws test 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,13 +3,7 @@ class Post < ActiveRecord::Base
     validates :user_id, presence: true
     validates :image, presence: true
 
-    has_attached_file :image, styles: { :medium => "640x" },
-                      :storage => :s3,
-                      :s3_credentials => {
-                                          :bucket => ENV['AWS_BUCKET'],
-                                          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                                          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                                          }
+    has_attached_file :image, styles: { :medium => "640x" }
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
     has_many :comments, dependent: :destroy
