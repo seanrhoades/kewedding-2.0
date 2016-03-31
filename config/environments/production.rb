099e718074007dcca05b676b9a8313f03b20d5d0
Sean Rhoades <sean@seanrhoades.com>
2016-03-30 22:26:40 -0400

    fixed minor syntax error preventing heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   #Use AWS for paperclip uploads
   config.paperclip_defaults = {
-  :storage => :s3
+  :storage => :s3,
   :s3_host_name => 's3-website-us-east-1.amazonaws.com',
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
