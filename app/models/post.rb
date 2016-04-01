a16217db7757b0b9a6d3bdfe8b0d3461bc99b2cf
Sean Rhoades <sean@seanrhoades.com>
2016-03-31 22:05:12 -0400

    reverse order posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    belongs_to :user
+    belongs_to :user, :order => "created_at DESC"
     validates :user_id, presence: true
     validates :image, presence: true
 
