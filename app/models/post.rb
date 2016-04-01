fd33e7e5c532ad5549c849aaf3c7f6f249abe264
Sean Rhoades <sean@seanrhoades.com>
2016-03-31 23:47:29 -0400

    pagination test

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,13 +2,13 @@ class Post < ActiveRecord::Base
     belongs_to :user
     validates :user_id, presence: true
     validates :image, presence: true
-
     has_attached_file :image,
           styles: { :medium => "640x" },
           :storage => :s3,
           :bucket  => ENV['AWS_S3_BUCKET']
 
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    paginates_per 10
 
     has_many :comments, dependent: :destroy
 
